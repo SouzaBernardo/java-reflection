1e9d2555e9ffdbbcb7a99952da0be400807f2f8a
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2023-12-26 18:02:14 -0300

    remove comment

--- src/main/java/reflection/Main.java
+++ src/main/java/reflection/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-//            exampleConstructors();
-//            exampleConstructorByBuilder();
+            exampleConstructors();
+            exampleConstructorByBuilder();
             exampleMethod();
         } catch (Exception e) {
             throw new RuntimeException(e);
