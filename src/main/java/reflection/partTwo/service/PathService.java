fc6bb1011504f8bef66212d9e2ffc1329ee5b715
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-09 08:53:33 -0300

    feat: PathService with valid path controller

--- src/main/java/reflection/partTwo/service/PathService.java
+++ src/main/java/reflection/partTwo/service/PathService.java
@@ -5,6 +5,9 @@ import reflection.partTwo.response.PathResponse;
 
 public class PathService {
 
+    public static final String CONTROLLER = "Controller";
+    public static final String DOMAIN = "reflection.partTwo.domain.";
+    public static final String CONTROLLER_PATH = "reflection.partTwo.controller.";
     public static final String SLASH = "/";
 
     public PathResponse validPath(String path) {
@@ -18,21 +21,49 @@ public class PathService {
 
         String className = capitalize(pathParts[0]);
         if (pathParts.length == 1) {
-            return new PathResponse(className, null);
+            return new PathResponse(className, null, null);
         }
 
-        return new PathResponse(className, pathParts[1]);
+        return new PathResponse(className, pathParts[1], null);
     }
 
-    private static String capitalize(String className) {
+    public PathResponse validPathController(String path) {
+
+        if (!path.startsWith("/"))
+            throw new InvalidPathException(path + " shouldn't start without '/'");
+
+        var pathParts = path.trim()
+                .replaceFirst(SLASH, "")
+                .split(SLASH);
+        String simpleClassName = capitalize(pathParts[0]);
+        String classNameController = capitalizeToController(pathParts[0]);
+        if (pathParts.length == 1) {
+            return new PathResponse(classNameController, null, classNameController);
+        }
+
+        return new PathResponse(classNameController, pathParts[1], classNameController);
+    }
+
+    private static String capitalizeToController(String className) {
         if (className.isBlank())
-            throw new InvalidPathException(className+ " cannot be blank");
+            throw new InvalidPathException(className + " cannot be blank");
 
 
         var firstCharacter = Character.toUpperCase(className.charAt(0));
         var restString = className.substring(1);
 
-        return firstCharacter + restString;
+        return CONTROLLER_PATH + firstCharacter + restString + CONTROLLER;
+    }
+
+    private static String capitalize(String className) {
+        if (className.isBlank())
+            throw new InvalidPathException(className + " cannot be blank");
+
+
+        var firstCharacter = Character.toUpperCase(className.charAt(0));
+        var restString = className.substring(1);
+
+        return DOMAIN + firstCharacter + restString;
     }
 
 }
